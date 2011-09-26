0e93957f67d6c3033ec851759180dd03ee795e7e
andrewrjones <andrewjones86@googlemail.com>
2011-09-26 19:48:18 +0100

    tidy

--- lib/App/MP4Meta/Command/film.pm
+++ lib/App/MP4Meta/Command/film.pm
@@ -7,30 +7,29 @@ use App::MP4Meta -command;
 sub usage_desc { "film %o [file ...]" }
 
 sub opt_spec {
-  return (
-    [ "blortex|X",  "use the blortex algorithm" ],
-    [ "recheck|r",  "recheck all results"       ],
-  );
+    return (
+        [ "blortex|X", "use the blortex algorithm" ],
+        [ "recheck|r", "recheck all results" ],
+    );
 }
 
 sub validate_args {
-  my ($self, $opt, $args) = @_;
+    my ( $self, $opt, $args ) = @_;
 
- 
-  # we need at least one file to work with
-  $self->usage_error("too few arguments") unless @$args;
-  
-  # check each file
-  for my $f (@$args){
-      unless( -e $f ){
-          $self->usage_error("$f does not exist");
-      }
-      unless( -r $f ){
-          $self->usage_error("can not read $f");
-      }
-      
-      # TODO: is $f an mp4?
-  }
+    # we need at least one file to work with
+    $self->usage_error("too few arguments") unless @$args;
+
+    # check each file
+    for my $f (@$args) {
+        unless ( -e $f ) {
+            $self->usage_error("$f does not exist");
+        }
+        unless ( -r $f ) {
+            $self->usage_error("can not read $f");
+        }
+
+        # TODO: is $f an mp4?
+    }
 }
 
 sub execute {
@@ -38,7 +37,7 @@ sub execute {
 
     # load another package to do work?
     # require App::MP4Meta::Film;
-    
+
     # for each file, parse?
     print "Foo";
 }
