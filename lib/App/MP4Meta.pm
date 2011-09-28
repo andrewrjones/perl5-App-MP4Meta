0d7e90fad17254fa35adac0b29b4f212f61ed8fc
andrewrjones <andrewjones86@googlemail.com>
2011-09-28 21:46:09 +0100

    typo

--- lib/App/MP4Meta.pm
+++ lib/App/MP4Meta.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package App::MP4Meta;
 
-# ABSTRACT: Apply iTunes-like meta data to an mp4 film.
+# ABSTRACT: Apply iTunes-like meta data to an mp4 file.
 
 use App::Cmd::Setup -app;
 
