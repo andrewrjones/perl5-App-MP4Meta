b1b06ff8a319110baf13c4abf7487b24c1bd44c4
andrewrjones <andrewjones86@googlemail.com>
2011-10-09 21:16:54 +0100

    docs

--- lib/App/MP4Meta.pm
+++ lib/App/MP4Meta.pm
@@ -3,8 +3,25 @@ use warnings;
 
 package App::MP4Meta;
 
-# ABSTRACT: Apply iTunes-like meta data to an mp4 file.
+# ABSTRACT: Apply iTunes-like metadata to an mp4 file.
 
 use App::Cmd::Setup -app;
+
+=head1 DESCRIPTION
+
+The C<mp4meta> command applies iTunes-like metadata to an mp4 file. The metadata is obtained by parsing the filename and searching the Internet to find its title, description and cover image, amongst others.
+
+Currently, only the C<film> command has been implemented. TV Series support will be added soon.
+
+=head2 film
+
+The C<film> command parses the filename and searches the IMDB for film metadata. See L<App::MP4Meta::Command::film> for more information.
+
+=head1 TODO
+
+=for :list
+* Implement C<tv> command for TV Series
+
+=cut
 
 1;
