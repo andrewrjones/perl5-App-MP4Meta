eb01e05a2b60c7597230abfb808734bfc193a016
Andrew Jones <andrewjones86@googlemail.com>
2012-02-25 14:48:22 +0000

    docs

--- lib/App/MP4Meta/Source/Data/TVEpisode.pm
+++ lib/App/MP4Meta/Source/Data/TVEpisode.pm
@@ -14,3 +14,31 @@ use Object::Tiny qw(
 );
 
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+  my $episode = App::MP4Meta::Source::Data::TVEpisode->new(%data);
+
+=attr cover
+
+Path to cover imaage.
+
+=attr genre
+
+Genre.
+
+=attr overview
+
+Overview or description of TV episode.
+
+=attr title
+
+Episode title.
+
+=attr year
+
+Year of episodes original broadcast
+
+=cut
