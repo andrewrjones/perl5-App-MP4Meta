51f1f3481997c820229a503096fe13bc3c8392cd
Andrew Jones <andrewjones86@googlemail.com>
2012-02-25 15:25:12 +0000

    tidy

--- lib/App/MP4Meta/Source/Data/TVEpisode.pm
+++ lib/App/MP4Meta/Source/Data/TVEpisode.pm
@@ -6,11 +6,11 @@ package App::MP4Meta::Source::Data::TVEpisode;
 # ABSTRACT: Contains data for a TV Episode.
 
 use Object::Tiny qw(
-    cover
-    genre
-    overview
-    title
-    year
+  cover
+  genre
+  overview
+  title
+  year
 );
 
 1;
