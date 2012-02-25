2f69eccaa7b36879aa71da6a7d1de42636b4fc0e
Andrew Jones <andrewjones86@googlemail.com>
2012-02-25 14:46:00 +0000

    add App::MP4Meta::Source::Data::TVEpisode

---
+++ lib/App/MP4Meta/Source/Data/TVEpisode.pm
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+package App::MP4Meta::Source::Data::TVEpisode;
+
+# ABSTRACT: Contains data for a TV Episode.
+
+use Object::Tiny qw(
+    cover
+    genre
+    overview
+    title
+    year
+);
+
+1;
