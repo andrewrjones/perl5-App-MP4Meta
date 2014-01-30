21ff26529d13cdaad4ab35d5da528883cf9a8e61
Jim Graham <jim@jim-graham.net>
2014-01-30 12:48:56 -0500

    Allow for Plex season-episode rules
    See
    https://plexapp.zendesk.com/hc/en-us/articles/200220687-Naming-Series-Based-TV-Shows

--- lib/App/MP4Meta/TV.pm
+++ lib/App/MP4Meta/TV.pm
@@ -17,12 +17,19 @@ use App::MP4Meta::Source::Data::TVEpisode;
 # a list of regexes to try to parse the file
 my @file_regexes = (
     qr/^S(?<season>\d)-E(?<episode>\d)\s+-\s+(?<show>.*)$/,
+    qr/^S(?<season>\d)-E(?<episode>\d)\s*-\s*E(?<end_episode\d)\s+-\s+(?<show>.*)$/,
     qr/^(?<show>.*)\s+S(?<season>\d\d)(\s|)E(?<episode>\d\d)$/,
+    qr/^(?<show>.*)\s+S(?<season>\d\d)(\s|)E(?<episode>\d\d)\s*-\s*E(?<end_episode>\d\d)$/,
     qr/^(?<show>.*)\.S(?<season>\d\d)E(?<episode>\d\d)/i,
+    qr/^(?<show>.*)\.S(?<season>\d\d)E(?<episode>\d\d)\s*-\s*E(?<end_episode>\d\d)/i,
     qr/^(?<show>.*)\s*-?\s*S(?<season>\d\d?)E(?<episode>\d\d?)/i,
+    qr/^(?<show>.*)\s*-?\s*S(?<season>\d\d?)E(?<episode>\d\d?)\s*-\s*E(?<end_episode>\d\d)/i,
     qr/^(?<show>.*)-S(?<season>\d\d?)E(?<episode>\d\d?)/,
-    qr/^(?<show>.*)_S(?<season>\d\d?)E(?<episode>\d\d?)/,
-    qr/S(?<season>\d\d?)E(?<episode>\d\d?)/,
+    qr/^(?<show>.*)-S(?<season>\d\d?)E(?<episode>\d\d?)\s*-\s*E(?<end_episode>\d\d)/,
+    qr/^(?<show>.*)-S(?<season>\d\d?)E(?<episode>\d\d?)/i,
+    qr/^(?<show>.*)-S(?<season>\d\d?)E(?<episode>\d\d?)\s*-\s*E(?<end_episode>\d\d)/i,
+    qr/S(?<season>\d\d?)E(?<episode>\d\d?)/i,
+    qr/S(?<season>\d\d?)E(?<episode>\d\d?)\s*-\s*E(?<end_episode>\d\d?)/i,
     qr/^(?<show>.*)\s*-?\s*(?<season>\d\d?)x(?<episode>\d\d?)/,
 );
 
